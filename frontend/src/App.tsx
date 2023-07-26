ba29a0f42f328aebcd879469612e37c301d59478
Elias Yazdani <elias_yazdani@yahoo.com>
2023-07-26 21:10:21 +0200

    add axios.get and player useState in App.tsx and integrationTest for GetMapping(gerAllPlayers) on ("/api/players")

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,30 @@
-
 import PlayerColumn from "./PlayerColumn.tsx";
 import {Player} from "./Player.ts";
 import "./App.css"
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 
 export default function App() {
 
-    const players: Player[] = [
-        {
-            "id": "1",
-            "firstName": "Timo",
-            "lastName": "B",
-            "age":35,
-        },
-        {
-            "id": "2",
-            "firstName": "Franck",
-            "lastName": "C",
-            "age":23,
-        },
-        {
-            "id": "3",
-            "firstName": "Janis",
-            "lastName": "H",
-            "age":40,
-        },
+    const [players, setPlayers] = useState<Player[]>([])
 
-    ]
+
+    function allPlayersList() {
+        axios.get("/api/players")
+            .then(response => {
+                setPlayers(response.data)
+            })
+    }
+
+    useEffect(allPlayersList, [])
+
 
     return (
         <>
             <h1 className="main-Title">Cup with me🏆</h1>
 
-            <PlayerColumn players={players} />
+            <PlayerColumn players={players}/>
 
         </>
     )
--- frontend/src/PlayerColumn.tsx
+++ frontend/src/PlayerColumn.tsx
@@ -61,26 +61,3 @@ export default function PlayerColumn(propsPlayerColumn : PropsPlayerColumn) {
     );
 }
 
-
-
-
-
-
-/*
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
-
-export default function PlayerTable() {
-    return (
-
-    );
-}*/
