6a5d7204e07737f86db3fb69aded7222059c9994
Elias Yazdani <elias_yazdani@yahoo.com>
2023-07-28 16:24:29 +0200

    add IntegrationTest for PostMapping(addNewPlayer)

--- backend/src/test/java/eliasyazdani/capstone/cupwithme/backend/player/PlayerIntegrationTest.java
+++ backend/src/test/java/eliasyazdani/capstone/cupwithme/backend/player/PlayerIntegrationTest.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -44,21 +44,18 @@ class PlayerIntegrationTest {
     @DirtiesContext
     @Test
     void whenAddNewPlayer_thenReturnNewPlayer() throws Exception {
-        String actual = mockMvc.perform(
-                post("/api/cup/players")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                                {"firstName": "testA","lastName": "testB","age": 25}
-                                """)
-        )
+        mockMvc.perform(
+                        post("/api/cup/players")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("""
+                                        {"firstName": "testA","lastName": "testB","age": 25}
+                                        """)
+                )
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                         {"firstName": "testA","lastName": "testB","age": 25}
                         """
-                ))
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
+                ));
 
 
     }
