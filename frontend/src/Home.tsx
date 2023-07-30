99df336176ae18e0a32fadbdb9a8384d51210cb7
Elias Yazdani <elias_yazdani@yahoo.com>
2023-07-30 22:53:34 +0200

    add Home page and send old Home page to PlayerList.tsx and add Router dependency. Reset input fields in newPlayerModal after cancel, edit Home page photo size

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,20 @@
+import Button from "@mui/material/Button";
+import {useNavigate} from "react-router-dom";
+
+
+export default function Home() {
+
+    const navigate = useNavigate();
+    return (
+        <>
+            <h1 style={{color: "red", textAlign: "center"}}>Cup With Me!!</h1>
+            <img style={{width: "100%"}}
+                 src="https://www.shutterstock.com/image-illustration/football-stadium-night-imaginary-modelled-600w-1912601503.jpg"
+                 alt="football"/>
+            <Button variant="contained" onClick={() => navigate("/players")}>
+                PlayerList
+            </Button>
+        </>
+
+    )
+}
