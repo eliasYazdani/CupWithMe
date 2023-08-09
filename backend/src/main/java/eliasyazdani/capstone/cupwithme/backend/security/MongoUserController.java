158b9992b66c4c714530a0e09a0cdad2f694216b
Elias Yazdani <elias_yazdani@yahoo.com>
2023-08-09 19:07:40 +0200

    Backend: use SecurityContextHolder in state of Principle in MongoUserController

--- backend/src/main/java/eliasyazdani/capstone/cupwithme/backend/security/MongoUserController.java
+++ backend/src/main/java/eliasyazdani/capstone/cupwithme/backend/security/MongoUserController.java
@@ -1,20 +1,16 @@
 package eliasyazdani.capstone.cupwithme.backend.security;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("/api/cup/users")
 public class MongoUserController {
 
     @GetMapping("me")
-    public String getUserInfo(Principal principal) {
-        if (principal == null) {
-            return "anonymousUser";
-        }
-        return principal.getName();
+    public String getUserInfo() {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
