f224546b95fdec187831a36e62fd4d690d9b8564
elias <elias@yahoo.com>
2023-11-01 13:20:12 +0100

    Frontend: add instruction and design to Homepage

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -45,12 +45,25 @@ export default function Home(propsHome: PropsHome) {
             <div className="logoutMode">
                 <div className="topSite">
                     <h1 className="main-Title">Cup With Me🏆</h1>
-                    <div className="login-signup">
-                        <LoginPage user={propsHome.user} onLogin={propsHome.onLogin}/>
-                        <Button onClick={() => navigate("/signup")} variant="contained"
-                                sx={{fontSize: "10px", padding: "5px 10px"}}>
-                            Sign up
-                        </Button>
+                    <div className="login-signup-instruction">
+                        <div className="login-signup">
+                            <LoginPage user={propsHome.user} onLogin={propsHome.onLogin}/>
+                            <Button onClick={() => navigate("/signup")} variant="contained"
+                                    sx={{fontSize: "10px", padding: "5px 10px"}}>
+                                Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <h2> 3 easy-peasy steps to goal </h2>
+                            <ol>
+                                <li> signup easily then will be logged-in</li>
+                                <li>add some new players</li>
+                                <li>create a tournament with basic Info and add the players into tournament  and enjoy it</li>
+                            </ol>
+                        </div>
+                        <div >
+
+                        </div>
                     </div>
                 </div>
                 <img className="image"
